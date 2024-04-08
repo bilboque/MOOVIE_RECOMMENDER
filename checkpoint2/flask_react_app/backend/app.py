7936b1c2bcb4952b76dd9f15c4f909f1939deba9
Timo Joannin <Timo.Joannin@etu.unige.ch>
2024-04-08 15:14:13 +0200

    mde

--- checkpoint2/flask_react_app/backend/app.py
+++ checkpoint2/flask_react_app/backend/app.py
@@ -9,7 +9,7 @@ connection = connector.connect(
     host="localhost",
     port=3306,
     user="root",
-    password="podmanroot13",
+    password="",
     database="DBMi"  # db name to access
 )
 
