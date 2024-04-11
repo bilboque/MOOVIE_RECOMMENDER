fa40d70c100590c6a57682457721f9ae133f692c
Timo Joannin <Timo.Joannin@etu.unige.ch>
2024-04-11 11:41:59 +0200

    Didk

--- checkpoint2/flask_react_app/backend/app.py
+++ checkpoint2/flask_react_app/backend/app.py
@@ -17,7 +17,7 @@ connection = mysql.connector.connect(
     host="localhost",
     port=3306,
     user="root",
-    password="",
+    password="podmanroot13",
     database="DBMi"  # db name to access
 )
 
