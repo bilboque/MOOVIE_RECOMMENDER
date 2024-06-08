ff14cd89378320d172d7b69dd3ac0d5aba7bdb85
Timo Joannin <Timo.Joannin@etu.unige.ch>
2024-06-08 18:40:44 +0200

    Started writing tests using pytest

--- checkpoint2/flaskr/temp_app.py
+++ checkpoint2/flaskr/temp_app.py
@@ -4,13 +4,14 @@ from db import db_blueprint
 from routes import routes_bp
 from api_routes import api_bp
 
-temp_app = Flask(__name__)
 
-
-temp_app.register_blueprint(auth_bp)
-temp_app.register_blueprint(db_blueprint)
-temp_app.register_blueprint(routes_bp)
-temp_app.register_blueprint(api_bp)
+def create_app():
+    temp_app = Flask(__name__)
+    temp_app.register_blueprint(auth_bp)
+    temp_app.register_blueprint(db_blueprint)
+    temp_app.register_blueprint(routes_bp)
+    temp_app.register_blueprint(api_bp)
+    return temp_app
 
 
 def get_session_key():
@@ -22,7 +23,7 @@ def get_session_key():
         return None
 
 
-temp_app.secret_key = get_session_key()  # secret key for session management
-
 if __name__ == '__main__':
+    temp_app = create_app()
+    temp_app.secret_key = get_session_key()  # secret key for session management
     temp_app.run(debug=True)
