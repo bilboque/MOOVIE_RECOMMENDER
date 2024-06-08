b64614a4e2ec6ff7de78f5ccc6b9f81f0aa64819
Timo Joannin <Timo.Joannin@etu.unige.ch>
2024-06-08 21:27:31 +0200

    one more test

--- checkpoint2/flaskr/test_route.py
+++ checkpoint2/flaskr/test_route.py
@@ -1,3 +1,8 @@
 def test_home(client):
     response = client.get("/")
     assert response.status_code == 200
+
+
+def test_search(client):
+    response = client.get("/search?query=StarWars")
+    assert response.status_code == 200
