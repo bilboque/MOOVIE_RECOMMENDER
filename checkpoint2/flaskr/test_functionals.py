ef02fab56f3d34ba9b9ba97b1fc1dc9c261958d5
Timo Joannin <Timo.Joannin@etu.unige.ch>
2024-06-18 10:59:44 +0200

    new tests

--- checkpoint2/flaskr/test_functionals.py
+++ checkpoint2/flaskr/test_functionals.py
@@ -26,38 +26,6 @@ def test_advanced_search(client):  # verifies the advanced search functionality
     assert response.status_code == 200
 
 
-def test_review(client):
-    client.get("/logout")
-    response = client.post(
-        "/review/11", data={"query": "Test: best movie ever"})
-    assert response.status_code == 302
-
-
-def test_login(client):
-    login_resp = client.get("login")
-    assert login_resp.status_code == 200
-    response = client.post(
-        "/login", data={"username": "testuser", "password": "testpassword"})
-    assert response.status_code == 302
-
-
-def test_watchlist_authenticated(client):
-    # Log in a user (assuming you have a user with id=1)
-    client.post(
-        "/login", data={"username": "testuser", "password": "testpassword"})
-    # Make a request to the /watchlist route
-    response = client.get("/watchlist")
-
-    # Check that the response status code is 200
-    assert response.status_code == 200
-
-
-def test_add(client):
-    client.get("/logout")
-    response = client.post("/add/3")
-    assert response.status_code == 302
-
-
 def test_db(client):
     with client.application.app_context():
         db = get_db_connection()
